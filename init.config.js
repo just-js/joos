cd476672105819cea8e295b127608d811d42c14a
Andrew Johnston <apjohnsto@gmail.com>
2024-05-25 07:07:32 +0100

    new init js

--- init.config.js
+++ init.config.js
@@ -1,14 +1,18 @@
-const bindings = ['core', 'fsmount']
-const libs = []
+
+const bindings = ['core', 'bestlines', 'fsmount']
+const libs = ['lib/repl.js', 'lib/ansi.js', 'lib/proc.js', 'lib/path.js', 'lib/stringify.js']
 const embeds = []
-const target = 'init' 
-const link_type = '-static'
+
+const target = 'init'
 const opt = '-O3 -march=native -mtune=native'
+
 const v8_opts = {
   v8_cleanup: 0, v8_threads: 1, on_exit: 0,
-//  v8flags: '--lite-mode --jitless --single-threaded --disable-write-barriers --no-verify-heap --no-expose-wasm --memory-reducer --optimize-for-size --stack-trace-limit=10 --use-strict --turbo-fast-api-calls --max-heap-size 64'
-  v8flags: '--stack-trace-limit=10 --use-strict --turbo-fast-api-calls --max-heap-size 16'
+  v8flags: '--lite-mode --jitless --single-threaded --disable-write-barriers --no-verify-heap --no-expose-wasm --memory-reducer --optimize-for-size --stack-trace-limit=10 --use-strict --turbo-fast-api-calls --max-heap-size 32'
+//  v8flags: '--stack-trace-limit=10 --use-strict --turbo-fast-api-calls --no-freeze-flags-after-init --cppgc-young-generation'
 }
-const main = 'init.js'
 
-export default { bindings, libs, embeds, target, link_type, opt, v8_opts, main }
+let link_type = '-static'
+
+const index = 'init.js'
+export default { bindings, libs, embeds, target, opt, v8_opts, link_type, index }
--- init.js
+++ init.js
@@ -1,116 +1,41 @@
-const { core } = lo.library('core')
-const { fsmount } = lo.library('fsmount')
+import { repl } from 'lib/repl.js'
 
-const { wrapMemory, utf8Decode, utf8Encode, utf8EncodeInto } = lo
-const {
-  O_RDWR, O_SYNC, O_CLOEXEC, PROT_WRITE, MAP_SHARED, LINUX_REBOOT_CMD_HALT, 
-  MS_SYNC, O_RDONLY, S_IRWXU, S_IRWXG, S_IROTH,
-  open, close, reboot, msync, getpagesize, exit, pread, mkdir
+const { fsmount } = lo.load('fsmount')
+const { core, assert, wrap_memory } = lo
+const { open, getpagesize, mmap, close, reboot, munmap, mkdir } = core
+const { 
+  O_RDWR, O_SYNC, O_CLOEXEC, PROT_WRITE, MAP_SHARED, LINUX_REBOOT_CMD_RESTART,
+  S_IRWXU, S_IRWXG, S_IROTH
 } = core
 const { mount } = fsmount
-const dir_flags = S_IRWXU | S_IRWXG | S_IROTH
 
-function findmem (str) {
-  const space = ' '
-  let spaces = 0
-  let last = 0
-  while (spaces < 24) {
-    const i = str.indexOf(space, last)
-    if (i > 0) {
-      if (spaces++ === 23) return (Number(str.slice(last, i)) * 4096)
-      last = i + 1
-    } else {
-      break
-    }
-  }
+const PORT_ADDRESS = Math.pow(2, 32) - (768 << 20)
+
+function mmio_signal () {
+  const mem_fd = open('/dev/mem', O_RDWR | O_SYNC | O_CLOEXEC, 0)
+  assert(mem_fd > 0)
+  const page_size = getpagesize()
+  const addr = mmap(0, page_size, PROT_WRITE, MAP_SHARED, mem_fd, PORT_ADDRESS)
+  const bytes = wrap_memory(addr, page_size)
+  bytes[0] = 123
+  munmap(addr, page_size)
+  close(mem_fd)
 }
 
-function wrap (handle, fn, plen = 0) {
-  const call = fn
-  const params = (new Array(plen)).fill(0).map((_, i) => `p${i}`).join(', ')
-  const f = new Function(
-    'handle',
-    'call',
-    `return function ${fn.name} (${params}) {
-    call(${params}${plen > 0 ? ', ' : ''}handle);
-    const v = handle[0] + ((2 ** 32) * handle[1])
-    return handle[0] + ((2 ** 32) * handle[1]);
-  }`,)
-  const fun = f(handle, call)
-  if (fn.state) fun.state = fn.state
-  return fun
-}
+// mount procfs
+assert(mkdir('/proc', S_IRWXU | S_IRWXG | S_IROTH) === 0);
+assert(mount('proc', '/proc', 'proc', 0, 0) === 0);
+assert(mount('tmpfs', '/tmp', 'tmpfs', 0, 0) === 0);
 
-function assert (condition, message, ErrorType = Error) {
-  if (!condition) {
-    if (message && message.constructor.name === 'Function') {
-      throw new ErrorType(message(condition))
-    }
-    throw new ErrorType(message || "Assertion failed")
-  }
-  return condition
-}
+const encoder = new TextEncoder()
+lo.core.write_file("/tmp/foo.txt", encoder.encode("hello"))
+const bytes = lo.core.read_file("/tmp/foo.txt")
+assert(bytes.length === 5)
 
-function addr (u32) {
-  return u32[0] + ((2 ** 32) * u32[1])  
-}
+// write to mmio device to signal vmm we have started
+mmio_signal()
+// launch a repl and wait for it
+await repl()
 
-function ptr (u8) {
-  u8.ptr = getAddress(u8)
-  u8.size = u8.byteLength
-  return u8
-}
-
-function mem () {
-  if (pread(fd, buf, 1024, 0) > 0) return findmem(decoder.decode(buf))
-  return 0
-}
-
-class TextEncoder {
-  encoding = 'utf-8'
-
-  encode (input = '') {
-    return utf8Encode(input)
-  }
-
-  encodeInto (src, dest) {
-    return utf8EncodeInto(src, dest)
-  }
-}
-
-class TextDecoder {
-  encoding = 'utf-8'
-
-  decode (u8) {
-    if (!u8.ptr) ptr(u8)
-    return utf8Decode(u8.ptr, u8.size)
-  }
-}
-
-const u32 = new Uint32Array(2)
-const getAddress = wrap(u32, lo.getAddress, 1)
-const mmap = wrap(u32, core.mmap, 6)
-const MAGIC_VALUE_SIGNAL_GUEST_BOOT_COMPLETE = 123
-const FIRST_ADDR_PAST_32BITS = Math.pow(2, 32)
-const MEM_32BIT_GAP_SIZE = 768 << 20
-const mem_fd = open('/dev/mem', O_RDWR | O_SYNC | O_CLOEXEC, 0)
-const mapped_size = getpagesize()
-const io_ptr = mmap(0, mapped_size, PROT_WRITE, MAP_SHARED, mem_fd, 
-  FIRST_ADDR_PAST_32BITS - MEM_32BIT_GAP_SIZE)
-const bytes = new Uint8Array(wrapMemory(io_ptr, mapped_size))
-bytes[0] = MAGIC_VALUE_SIGNAL_GUEST_BOOT_COMPLETE
-msync(io_ptr, mapped_size, MS_SYNC)
-const bytes_32 = new Uint32Array(bytes.buffer)
-const decoder = new TextDecoder()
-const buf = new Uint8Array(1024)
-assert(mkdir('/proc', dir_flags) === 0)
-assert(mount('proc', '/proc', 'proc', 0, 0) === 0)
-const fd = open(`/proc/self/stat`, O_RDONLY)
-assert(fd > 0)
-bytes[0] = MAGIC_VALUE_SIGNAL_GUEST_BOOT_COMPLETE
-lo.print(`${mem()}\n`)
-msync(io_ptr, mapped_size, MS_SYNC)
-close(mem_fd)
-close(fd)
-//reboot(LINUX_REBOOT_CMD_HALT)
-exit(0)
+// if we do this, we don't get a kernel panic
+reboot(LINUX_REBOOT_CMD_RESTART)
